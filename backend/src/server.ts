143b6f0a4b2b9ceef782bb9cfd93c64c8fddad57
renezoka <rmmedeiros300@gmail.com>
2026-03-30 17:21:03 -0300

    baixando as dependencias do supabase

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.routes';
+import {createClient} from '@supabase/supabase-js';
 
 dotenv.config();
 
