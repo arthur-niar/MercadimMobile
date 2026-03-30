5084302d1c28fb3fc8a29a6bd95a8f02502f63ed
renezoka <rmmedeiros300@gmail.com>
2026-03-30 18:24:37 -0300

    adicionando get e put de produtos

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,6 +4,10 @@ import dotenv from 'dotenv';
 import authRoutes from './routes/auth.routes';
 import {createClient} from '@supabase/supabase-js';
 
+const supabaseUrl = process.env.SUPABASE_URL || '';
+const supabaseKey = process.env.SUPABASE_KEY || '';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
 dotenv.config();
 
 const app = express();
@@ -17,12 +21,37 @@ app.use((req, _res, next) => {
   next();
 });
 
+
 app.use('/api/auth', authRoutes);
 
 app.get('/health', (_req, res) => {
   res.json({ status: 'ok', message: 'Mercadim API funcionando' });
 });
 
+app.get('/usuarios', async (_req, res) => {
+
+  res.status(200).json({ message: 'Lista de usuários' });
+  const { data, error } = await supabase.from('usuarios').select('*');
+
+  if (error) {
+    console.error('Erro ao buscar usuários:', error);
+    return res.status(500).json({ error: 'Erro ao buscar usuários' });
+  }
+  res.json(data);
+});
+
+app.put('/usuarios/:id', async (req, _res) => {
+
+  const {id} = req.params;
+  const {nome, email, senha, url} = req.body;
+
+  const { data, error } = await supabase
+    .from('usuarios')
+    .update({ nome, email, senha, url })
+    .eq('id', id);
+});
+
+  
 app.listen(PORT, () => {
   console.log(`Servidor: http://localhost:${PORT}`);
   console.log(`Teste: teste@mercadim.com / senha123`);
